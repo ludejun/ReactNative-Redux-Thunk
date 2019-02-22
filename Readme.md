fe2f42d01c814600b1093d4fa15e65c31d57d87a
ludejun <ludejun@meituan.com>
2019-02-22 21:28:14 +0800

    add remote-redux-devtools，使得可以在chrome中远程调试redux

--- Readme.md
+++ Readme.md
@@ -1,4 +1,10 @@
 # ReactNative-Redux
 ludejun@live.cn
 
-集成ReactNative、Navigation、Eslint、Prettier的简易工程化项目。是初级RN工程化项目基础，适合初学者学习或极简demo。
+集成ReactNative、Redux、redux-devtools、Navigation、Eslint、Prettier的工程化项目。适合简单项目及学习。
+
+打开redux-devtools的方法：
+1. 打开RN远程调试
+2. chrome安装有redux-devtools
+3. 在远程页（一般为：http://localhost:8081/debugger-ui/），右键-Redux Devtools-Open Remote Devtools
+即可获得和react开发一样的redux-devtools界面
--- src/Root.js
+++ src/Root.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import { devToolsEnhancer } from 'redux-devtools-extension';
+import devToolsEnhancer from 'remote-redux-devtools';
 import reducers from './reducers';
 import App from './App';
 
-const store = createStore(
-  reducers, /* preloadedState, */
-  devToolsEnhancer(
-    // Specify name here, actionsBlacklist, actionsCreators and other options if needed
-  )
-);
+const store = createStore(reducers, devToolsEnhancer());
+
+if (module.hot) {
+  // Enable hot module replacement for reducers
+  module.hot.accept(() => {
+    const nextRootReducer = require('./reducers/index').default;
+    store.replaceReducer(nextRootReducer);
+  });
+}
+
 
 export default () => (
   <Provider store={store}>
