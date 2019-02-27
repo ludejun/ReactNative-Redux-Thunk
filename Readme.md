3b1263674e8032a1b290e913f9e2ab614fbb903c
ludejun <ludejun@meituan.com>
2019-02-27 15:51:55 +0800

    add codepush & change readme

--- Readme.md
+++ Readme.md
@@ -1,7 +1,7 @@
 # ReactNative-Redux-Thunk
 ludejun@live.cn
 
-集成ReactNative、Redux、Thunk、redux-devtools、Navigation、Eslint、Prettier的工程化项目。适合复杂项目开展。
+集成ReactNative、Redux、Thunk、CodePush、redux-devtools、Navigation、Eslint、Prettier的工程化项目。适合复杂项目开展。
 
 ### 打开redux-devtools的方法：
 1. 打开RN远程调试（模拟器上打开Menu面板，选择远程调试）
@@ -25,3 +25,34 @@ ludejun@live.cn
     // in project directory
     
        react-native-rename <New_Project_ Name>
+
+
+### code-push
+参考：https://github.com/Microsoft/react-native-code-push
+
+ReactNativeReduxThunk
+┌────────────┬──────────────────────────────────────────────────────────────────┐
+│ Name       │ Deployment Key                                                   │
+├────────────┼──────────────────────────────────────────────────────────────────┤
+│ Production │ KUWxBVCPgQMTINEb9ZCZQERghzTBea84ca97-2860-4663-b17c-f29998a1c45d │
+├────────────┼──────────────────────────────────────────────────────────────────┤
+│ Staging    │ iAi7KDiudvIGysnM8WWjmMXJXuspea84ca97-2860-4663-b17c-f29998a1c45d │
+└────────────┴──────────────────────────────────────────────────────────────────┘
+ReactNativeReduxThunkAndroid
+┌────────────┬──────────────────────────────────────────────────────────────────┐
+│ Name       │ Deployment Key                                                   │
+├────────────┼──────────────────────────────────────────────────────────────────┤
+│ Production │ ltk1qpwwtsktwD64fUgVyDvCIJjvea84ca97-2860-4663-b17c-f29998a1c45d │
+├────────────┼──────────────────────────────────────────────────────────────────┤
+│ Staging    │ uhSDFY5g7NErTVC0i6Z1O5VMhJYZea84ca97-2860-4663-b17c-f29998a1c45d │
+└────────────┴──────────────────────────────────────────────────────────────────┘
+
+code-push release-react ReactNativeReduxThunk ios --plistFile "ios/ReactNativeReduxThunk/Info.plist" --development
+
+
+#### Issues
+1. undefined is not an object (evaluating '_reactNativeCodePush.default.CheckFrequency')
+    - react-native link
+    - react-native run-ios
+
+
--- src/Root.js
+++ src/Root.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import { composeWithDevTools } from 'remote-redux-devtools';
+import CodePush from 'react-native-code-push'; // 引入code-push
 import reducers from './reducers';
 import App from './App';
 
@@ -26,8 +27,63 @@ if (module.hot) {
   });
 }
 
-export default () => (
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+// code-push用法： https://github.com/Microsoft/react-native-code-push
+const codePushOptions = {
+  // 设置检查更新的频率
+  // ON_APP_RESUME APP恢复到前台的时候
+  // ON_APP_START APP开启的时候
+  // MANUAL 手动检查
+  checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
+};
+
+class Root extends Component {
+  // 如果有更新的提示
+  static syncImmediate() {
+    CodePush.sync({
+      // 安装模式
+      // ON_NEXT_RESUME 下次恢复到前台时
+      // ON_NEXT_RESTART 下一次重启时
+      // IMMEDIATE 马上更新
+      installMode: CodePush.InstallMode.IMMEDIATE,
+      // 对话框
+      updateDialog: {
+        // 是否显示更新描述
+        appendReleaseDescription: true,
+        // 更新描述的前缀。 默认为"Description"
+        descriptionPrefix: '更新内容：',
+        // 强制更新按钮文字，默认为continue
+        mandatoryContinueButtonLabel: '立即更新',
+        // 强制更新时的信息. 默认为"An update is available that must be installed."
+        mandatoryUpdateMessage: '必须更新后才能使用',
+        // 非强制更新时，按钮文字,默认为"ignore"
+        optionalIgnoreButtonLabel: '稍后',
+        // 非强制更新时，确认按钮文字. 默认为"Install"
+        optionalInstallButtonLabel: '后台更新',
+        // 非强制更新时，检查到更新的消息文本
+        optionalUpdateMessage: '有新版本了，是否更新？',
+        // Alert窗口的标题
+        title: '更新提示',
+      },
+    });
+  }
+
+  componentWillMount() {
+    CodePush.disallowRestart(); // 页禁止重启
+    this.syncImmediate(); // 开始检查更新
+  }
+
+  componentDidMount() {
+    CodePush.allowRestart(); // 在加载完了，允许重启
+    console.log('🎉🎉🎉🔥');
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+  }
+}
+
+export default CodePush(codePushOptions)(Root);
